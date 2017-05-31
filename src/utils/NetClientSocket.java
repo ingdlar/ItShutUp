d0c6710af6968d40d346667e569013220dcc65a9
ingdlar <juancarlosdlar@gmail.com>
2017-05-31 15:11:00 -0400

    Fist

---
+++ src/utils/NetClientSocket.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author E1002498
+ */
+public class NetClientSocket {
+
+    String dstAddress;
+    int dstPort;
+    String response = "";
+
+    NetClientSocket(String addr, int port) {
+        dstAddress = addr;
+        dstPort = port;
+        //this.textResponse = textResponse;
+    }
+    
+    protected String doInBackground(Object... arg0) {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket(dstAddress, dstPort);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            InputStream inputStream = socket.getInputStream();
+
+			/*
+             * notice: inputStream.read() will block if no data return
+			 */
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+                response += byteArrayOutputStream.toString("UTF-8");
+            }
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "UnknownHostException: " + e.toString();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "IOException: " + e.toString();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response;
+    }
+   
+    protected void onPostExecute(String result) {
+        //textResponse.setText(response);
+        //super.onPostExecute(result);
+    }
+}
