22b509d3d478db895256bd04aaed894fd47dd936
ingdlar <juancarlosdlar@gmail.com>
2017-06-01 14:32:26 -0400

    Fist

--- src/itshutup/ItShutUp.java
+++ src/itshutup/ItShutUp.java
@@ -5,6 +5,12 @@
  */
 package itshutup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author E1002498
@@ -14,8 +20,17 @@ public class ItShutUp {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        String serverAddress = JOptionPane.showInputDialog(
+            "Enter IP Address of a machine that is\n" +
+            "running the date service on port 9090:");
+        Socket s = new Socket(serverAddress, 9090);
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String answer = input.readLine();
+        JOptionPane.showMessageDialog(null, answer);
+        System.exit(0);
     }
     
 }
